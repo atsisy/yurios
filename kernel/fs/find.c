8e7f1d05069128686c6ebb7934f9a00fc9f69fea
Akihiro Takai <at.sisy@gmail.com>
2016-12-05 22:35:00 +0900

    ファイル検索関数を定義

---
+++ kernel/fs/find.c
@@ -0,0 +1,43 @@
+#include "../../include/yrfs.h"
+
+/*
+ *=======================================================================================
+ *ffind関数
+ *ファイルを検索する関数
+ *=======================================================================================
+ */
+i32_t ffind(char *pathname) {
+	u32_t i, j;
+	_Bool w_flag;
+	struct i_node inode;
+
+	/*
+	 *ファイルの中身領域に到達するまで走査
+	 */
+	for(i = 0;i < __FILE_OBJECT_ZONE__;i++){
+		//inode情報を読み取る
+		iread(&inode, i);
+		/*
+		 *もしも最初の文字があっていたら
+		 */
+		if(*inode.file_name == *pathname){
+			/*
+			 *確かめる
+			 */
+			for(j = 0, w_flag = 0 ;pathname[j] != '\0';j++){
+				if(pathname[j] != inode.file_name[j])  //違った
+					w_flag = 1;
+			}
+			/*
+			 *あった
+			 */
+			if(!w_flag)
+				return i;
+		}
+	}
+
+	/*
+	 *だめでしたわ〜
+	 */
+	return -1;
+}
--- kernel/fs/open.c
+++ kernel/fs/open.c
@@ -4,15 +4,19 @@
 int do_open(char *pathname, u32_t flags) {
 
 	int fd = 0;
+	struct i_node inode;
+	
 	if(flags & __O_CREAT__){
 		/*
 		 *inode作成
 		 */
-		struct i_node inode = icreat(pathname);
+		inode = icreat(pathname);
 		/*
 		 *inodeを補助記憶装置に書き出し
 		 */
 		fd = iwrite(&inode);
+	}else if(flags & __O_RDONLY__){
+		
 	}
 
 	return fd;
