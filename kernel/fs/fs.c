32177367da6e0a73b1cb6c3e3e70f30dd1af0a24
Akihiro Takai <at.sisy@gmail.com>
2016-12-06 22:01:18 +0900

    ファイルシステムの初期化を真面目にやるようにした

--- kernel/fs/fs.c
+++ kernel/fs/fs.c
@@ -4,13 +4,18 @@
  *=======================================================================================
  */
 #include "../../include/yrfs.h"
-#include "../../include/string.h"
 
 struct block_info blocks_info[100];
 
 void init_yrfs() {
 	u32_t i = 0;
+	struct i_node inode;
+	
 	for(;i < 100;i++){
-		blocks_info[i].empty = 1;
+		iread(&inode, i);
+		/*
+		 *ファイル名の先頭がヌル文字のとき空と定義する
+		 */
+		blocks_info[i].empty = inode.file_name[0] == '\0';
 	}
 }
