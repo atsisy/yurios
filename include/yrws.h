1bdf80332b67169e5f19eb762390eaea6fc5b09b
AkihiroTakai <at.sisy@gmail.com>
2016-12-16 22:03:32 +0900

    レイヤーとレイヤーを管理するための構造体を定義(yrws)

--- include/yrws.h
+++ include/yrws.h
@@ -4,6 +4,14 @@
 #include "types.h"
 
 /*
+*デフォルトの背景色
+*/
+#define __DEFAULT_WALLPAPER_COLOR__ 54
+/*
+*レイヤーの最大数
+*/
+#define __LAYER_LIMIT__ 1024
+/*
 *マウスのデータとかをひとまとめにした構造体
 */
 struct MOUSE_INFO {
@@ -23,9 +31,29 @@ struct MOUSE_CURSOR {
 };
 
 /*
-*デフォルトの背景色
+*重ね合わせ処理に用いるレイヤー構造体
 */
-#define __DEFAULT_WALLPAPER_COLOR__ 54
+struct Layer {
+	u8_t *data;		//レイヤーのデータ
+	u16_t width;	//レイヤーの幅
+	u16_t height;	//レイヤーの高さ
+	u16_t display_x;	//レイヤーがディスプレイのどこにいるか
+	u16_t display_y;	//レイヤーがディスプレイのどこにいるか
+	u16_t position;	//レイヤーの表面からの順番
+	u32_t invisible;	//透明色
+	u32_t flags;	//フラグ
+};
+
+/*
+*
+*/
+struct Layer_Master {
+	i32_t top_layer;
+	struct Layer layers[__LAYER_LIMIT__];
+	struct Layer *layers_pointers[__LAYER_LIMIT__];
+};
+
+
 
 /*
 *マウスを初期化する関数
--- yrws/yrws_main.c
+++ yrws/yrws_main.c
@@ -101,7 +101,11 @@ static void init_yrws(void){
 }
 
 static void draw_cursor(void){
-      boxfill8(binfo->vram, binfo->scrnx, __RGB256COL__(255, 255, 255), cursor.x, cursor.y, cursor.x+5, cursor.y+5);
+      //boxfill8(binfo->vram, binfo->scrnx, __RGB256COL__(255, 255, 255), cursor.x, cursor.y, cursor.x+5, cursor.y+5);
+      u32_t i;
+      for(i = 0;i < 12;i++){
+            binfo->vram[(binfo->scrnx*(cursor.y+i))+cursor.x] = 0;
+      }
 }
 
 static void erase_cursor(void){
