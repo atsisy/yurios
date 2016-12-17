e9d4e00d1adfd29cd0a988243cf6e70b5307ad08
AkihiroTakai <at.sisy@gmail.com>
2016-12-17 21:55:15 +0900

    再描画領域の限界を定義

--- yrws/layer_redraw.c
+++ yrws/layer_redraw.c
@@ -22,11 +22,20 @@ void redraw_all_layer(struct Layer_Master *master, struct Layer *layer, u16_t st
  *redraw_all_layerで、範囲を指定し高速化した関数
  *=======================================================================================
  */
-void redraw_layers(struct Layer_Master *master, u16_t start_x, u16_t start_y, u16_t end_x, u16_t end_y){
+void redraw_layers(struct Layer_Master *master, i16_t start_x, i16_t start_y, i16_t end_x, i16_t end_y){
 
       i32_t h, x, y, display_x, display_y, start_x_sub, start_y_sub, end_x_sub, end_y_sub;
 	unsigned char *buf, *vram = binfo->vram, c;
 	struct Layer *layer;
+
+      if(start_x < 0)
+            start_x = 0;
+      if(start_y < 0)
+            start_y = 0;
+      if(end_x > binfo->scrnx)
+            end_x = binfo->scrnx;
+      if(end_y > binfo->scrny)
+            end_y = binfo->scrny;
 
       for(h = 0;h <= master->top_layer;h++){
 
