6c043df0857ca61c13b4b876096486cb5fdad61f
AkihiroTakai <at.sisy@gmail.com>
2016-12-17 12:44:07 +0900

    マウスカーソルをまともに描画するようにした
    yurifontを使ってマウスカーソルの16進数表示を得て、配列にぶち込んだ

--- yrws/cursor.c
+++ yrws/cursor.c
@@ -3,6 +3,9 @@
 
 extern struct MOUSE_CURSOR cursor;
 
+static char MOUSE_CURSOR_IMAGE[16] = {
+      0x80, 0xc0, 0xe0, 0xf8, 0xfc, 0xfe, 0xff, 0xff, 0xff, 0xff, 0xff, 0x1e, 0x0e, 0x0e, 0x06, 0x03
+};
 /*
  *=======================================================================================
  *draw_cursor関数
@@ -10,13 +13,32 @@ extern struct MOUSE_CURSOR cursor;
  *=======================================================================================
  */
 void draw_cursor(void){
-      //boxfill8(binfo->vram, binfo->scrnx, __RGB256COL__(255, 255, 255), cursor.x, cursor.y, cursor.x+5, cursor.y+5);
       u32_t i;
-      for(i = 0;i < 12;i++){
-            binfo->vram[(binfo->scrnx*(cursor.y+i))+cursor.x] = 0;
-      }
+      char point, *vram;
+
+      for(i = 0;i < 16;i++){
+		vram = (char *)(binfo->vram) + ((cursor.y + i) * binfo->scrnx) + cursor.x;
+            point = MOUSE_CURSOR_IMAGE[i];
+
+		if ((point & 0x80) != 0)
+			vram[0] = BLACK;
+		if ((point & 0x40) != 0)
+			vram[1] = BLACK;
+		if ((point & 0x20) != 0)
+			vram[2] = BLACK;
+		if ((point & 0x10) != 0)
+			vram[3] = BLACK;
+		if ((point & 0x08) != 0)
+			vram[4] = BLACK;
+		if ((point & 0x04) != 0)
+			vram[5] = BLACK;
+		if ((point & 0x02) != 0)
+			vram[6] = BLACK;
+		if ((point & 0x01) != 0)
+			vram[7] = BLACK;
+	}
 }
 
 void erase_cursor(void){
-      boxfill8(binfo->vram, binfo->scrnx, __DEFAULT_WALLPAPER_COLOR__, cursor.x, cursor.y, cursor.x+5, cursor.y+5);
+      boxfill8(binfo->vram, binfo->scrnx, __DEFAULT_WALLPAPER_COLOR__, cursor.x, cursor.y, cursor.x+8, cursor.y+16);
 }
