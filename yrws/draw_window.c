a5b97dec58f320f4b5a9ecd2b261db79a1cf161a
AkihiroTakai <at.sisy@gmail.com>
2016-12-18 20:00:54 +0900

    ウィンドウのデモプログラムを修正
    ウィンドウの表示位置をマウスカーソルの位置にした

--- yrws/draw_window.c
+++ yrws/draw_window.c
@@ -15,7 +15,7 @@ void draw_window(i16_t start_x, i16_t start_y, i16_t width, i16_t height){
       boxfill8(window->data, width, __RGB256COL__(0, 0, 0), 48, 0, width, 16);
       boxfill8(window->data, width, __RGB256COL__(255, 255, 255), 0, 16, width, height);
 
-      move_layer(LAYER_MASTER, window, 100, 100);
+      move_layer(LAYER_MASTER, window, start_x, start_y);
       layer_ch_position(LAYER_MASTER, window, 2);
 
       redraw_all_layer(LAYER_MASTER, window, 0, 0, binfo->scrny, binfo->scrny);
