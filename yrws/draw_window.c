c07302cd528e918fea2372cde6e309e2c1d11712
AkihiroTakai <at.sisy@gmail.com>
2016-12-19 20:00:44 +0900

    draw_window関数が引数としてレイヤーのポインタを返すように編集

--- yrws/draw_window.c
+++ yrws/draw_window.c
@@ -1,7 +1,13 @@
 #include "../include/kernel.h"
 #include "../include/yrws.h"
 
-void draw_window(i16_t start_x, i16_t start_y, i16_t width, i16_t height){
+/*
+ *=======================================================================================
+ *draw_window関数
+ *ウィンドウを描画する関数
+ *=======================================================================================
+ */
+struct Layer *draw_window(i16_t start_x, i16_t start_y, i16_t width, i16_t height){
       struct Layer *window = layer_alloc(Yrws_Master.LAYER_MASTER);
       u8_t *window_buf = (u8_t *)memory_alloc_4k(memman, width * height);
       layer_chbuf(window, window_buf);
@@ -19,4 +25,6 @@ void draw_window(i16_t start_x, i16_t start_y, i16_t width, i16_t height){
 	puttext(window, "YURI OS window!!", 20, 20, __RGB256COL__(0, 0, 0));
 
       redraw_all_layer(Yrws_Master.LAYER_MASTER, window, 0, 0, binfo->scrny, binfo->scrny);
+
+	return window;
 }
