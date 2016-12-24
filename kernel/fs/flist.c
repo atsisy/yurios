baeab0cf902697aa90c6a7c8ec6c7c6ca0322235
AkihiroTakai <at.sisy@gmail.com>
2016-12-24 14:24:44 +0900

    flsコマンドでファイルのサイズを表示するようにした

--- kernel/fs/flist.c
+++ kernel/fs/flist.c
@@ -1,6 +1,8 @@
 #include "../../include/yrfs.h"
 #include "../../include/sh.h"
 
+static char *yrfs_file_info(char *info, struct i_node *inode);
+
 /*
  *=======================================================================================
  *file_list関数
@@ -9,17 +11,22 @@
  */
 void file_list(char *option) {
 	u32_t i;
-	struct i_node inode;
-	char inode_id[24];
+	struct i_node *inode = memory_alloc(memman, sizeof(struct i_node));
+	char *str = (char *)memory_alloc(memman, 270);
+	
 	for(i = 0;i < __INODE_LIMIT__;i++){
 		if(blocks_info[i].exist){
-			
-			iread(&inode, i);
-			print(inode.file_name);
-			sprintf(inode_id, " %d", inode.id);
-			puts(inode_id);
-
-			zeroclear_8array(inode_id, 24);
+			iread(inode, i);
+			zeroclear_8array(str, 270);
+			puts(yrfs_file_info(str, inode));
 		}
 	}
+
+	memory_free(memman, (u32_t)str, 270);
+	memory_free(memman, (u32_t)inode, sizeof(struct i_node));
+}
+
+static char *yrfs_file_info(char *info, struct i_node *inode){
+	sprintf(info, "%s size:%d inode:%d", inode->file_name, inode->size, inode->id);
+	return info;
 }
