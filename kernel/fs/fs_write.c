4e7ea6d8394f2f21ab1661368f11d903a0f76ca1
AkihiroTakai <at.sisy@gmail.com>
2016-12-24 13:52:38 +0900

    ファイル書き込み時にファイルサイズを更新するようにした

--- kernel/fs/fs_write.c
+++ kernel/fs/fs_write.c
@@ -10,12 +10,35 @@
  */
 u32_t yrfs_write(u32_t fd, char *buffer, u32_t bytes){
 
+	/*
+	 *作業用のinodeを確保
+	 */
 	struct i_node *inode = (struct i_node *)memory_alloc(memman, sizeof(struct i_node));
-	iread(inode, fd);
-	
-	write_ata_sector(&ATA_DEVICE0, inode->seek_address.sector, buffer, byte2sectors(bytes));
-	mark_used_sign(inode->begin_address.sector, inode->begin_address.sector+byte2sectors(bytes));
 
+	/*
+	 *書き込むファイルのinodeを読み込む
+	 */
+	iread(inode, fd);
+
+	/*
+	 *書き込む
+	 */
+	write_ata_sector(&ATA_DEVICE0, inode->seek_address.sector, buffer, byte2sectors(bytes));
+
+	/*
+	 *inodeを更新
+	 */
+	inode->size += (inode->seek_address.offset + bytes);
+	iupdate(inode);
+
+	/*
+	 *使用済みマークをつける
+	 */
+	mark_used_sign(inode->begin_address.sector, inode->begin_address.sector+byte2sectors(inode->size));
+
+	/*
+	 *メモリを開放
+	 */
 	memory_free(memman, (u32_t)inode, sizeof(struct i_node));
 
 	return byte2sectors(bytes);
