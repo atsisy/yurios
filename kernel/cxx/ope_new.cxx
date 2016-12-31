15cb3df8f96dbb799efa20e12d1aed2302d05525
AkihiroTakai <at.sisy@gmail.com>
2016-12-31 15:21:29 +0900

    C++のnew演算子を定義した

---
+++ kernel/cxx/ope_new.cxx
@@ -0,0 +1,36 @@
+#include "../../include/kernel.h"
+
+/*
+ *=======================================================================================
+ *new演算子を定義する
+ *=======================================================================================
+ */
+
+//new
+void *operator new(u32_t size){
+	void *memory = (void *)memory_alloc(memman, size);
+	return memory;
+}
+
+//new[]
+void *operator new[](u32_t size){
+	void *memory = (void *)memory_alloc(memman, size);
+	return memory;
+}
+
+
+/*
+ *=======================================================================================
+ *グローバル変数等で使用する配置new演算子
+ *=======================================================================================
+ */
+
+//配置new
+void *operator new(u32_t size, void *memory){
+	return memory;
+}
+
+//配置new[]
+void *operator new[](u32_t size, void *memory){
+	return memory;
+}
