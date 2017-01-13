e8d4ba152ddaab87b211e06b2bf7c8efcbc7c7fb
AkihiroTakai <at.sisy@gmail.com>
2017-01-13 18:36:04 +0900

    puttext関数内の再描画処理呼び出し部分のバグを修正

--- yrws/lib/puttext.c
+++ yrws/lib/puttext.c
@@ -4,12 +4,29 @@
 
 /*
  *=======================================================================================
+ *puttext関数
  *ウィンドウにテキストを表示する関数
+ *struct Layer *window
+ *描画するウィンドウ
+ *char *text
+ *描画する文字列
+ *u16_t x
+ *描画するX位置
+ *u16_t y
+ *描画するY位置
+ *char color
+ *色
  *=======================================================================================
  */
 char puttext(struct Layer *window, char *text, u16_t x, u16_t y, char color){
+	/*
+	 *文字列描画
+	 */
 	putfonts8_asc(window->data, window->width, x, 16+y, color, text);
+	/*
+	 *再描画処理
+	 */
 	redraw_all_layer(Yrws_Master.LAYER_MASTER, window,
-			     0, 0, /*window->display_x + x + (strlen(text) << 3)*/500, /*window->display_y + y + 32*/500);
+			     x, y, x + (strlen(text) << 3), y + 32);
 	return 0;
 }
