f92794e5e9cd5e2e5d2eddc80b6fe92831d299e1
AkihiroTakai <at.sisy@gmail.com>
2017-01-14 14:16:04 +0900

    ファイルを削除する関数を追加

---
+++ kernel/fs/fremove.c
@@ -0,0 +1,30 @@
+#include "../../include/kernel.h"
+#include "../../include/yrfs.h"
+#include "../../include/sh.h"
+#include "../../include/ata.h"
+
+/*
+ *=======================================================================================
+ *RemoveFile関数
+ *ファイルを削除する関数
+ *引数
+ *char *file_name
+ *削除するファイル名
+ *=======================================================================================
+ */
+u8_t RemoveFile(char *file_name){
+	struct i_node *inode = (struct i_node *)memory_alloc(memman, sizeof(struct i_node));
+
+	i32_t fd = do_open(file_name, __O_RDONLY__);
+	iread(inode, fd);
+
+	char zero[512] = { 0 };
+
+	do_write(fd, zero, inode->size);
+	write_ata_sector(&ATA_DEVICE0, fd, zero, 1);
+	blocks_info[fd].exist = __UNUSED_BLOCK__;
+
+	memory_free(memman, (u32_t)inode, sizeof(struct i_node));
+
+	return 0;
+}
--- lib/yuri/sh_cmd.c
+++ lib/yuri/sh_cmd.c
@@ -253,16 +253,7 @@ void command_writeyim(char *file_name){
  *=======================================================================================
  */
 void command_rm(char *file_name){
-	struct i_node *inode = (struct i_node *)memory_alloc(memman, sizeof(struct i_node));
-
-	i32_t fd = do_open(file_name, __O_RDONLY__);
-	iread(inode, fd);
-
-	char zero[512] = { 0 };
-
-	do_write(fd, zero, inode->size);
-	write_ata_sector(&ATA_DEVICE0, fd, zero, 1);
-	blocks_info[fd].exist = __UNUSED_BLOCK__;
+      RemoveFile(file_name);
 }
 
 /*
