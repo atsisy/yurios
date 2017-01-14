dd4d310cb6c1b0a4bef364abce53fea1f5b823fd
AkihiroTakai <at.sisy@gmail.com>
2017-01-14 16:04:26 +0900

    sysc.hにコメントを記述

--- include/sysc.h
+++ include/sysc.h
@@ -1,12 +1,22 @@
 #ifndef __SYSC_H__
 #define __SYSC_H__
 
+/*
+ *=======================================================================================
+ *システムコール関数のプロトタイプ宣言
+ *=======================================================================================
+ */
+
 #include "types.h"
 
 /*
-  システムコール関数のプロトタイプ宣言
-*/
+ *writeシステムコール
+ */
 size_t do_write(int fd, char *buf, int count);
+
+/*
+ *readシステムコール
+ */
 size_t do_read(int fd, char *buf, int count);
 
 /*
@@ -19,18 +29,38 @@ size_t do_read(int fd, char *buf, int count);
 u16_t do_gettime(char flag);
 
 /*
- *OS側のシステムコール実装
+ *sleepシステムコール
  */
 void do_sleep(unsigned int timeout);
 
+/*
+ *forkシステムコール（未完成）
+ */
 u32_t do_fork(void);
 
-
+/*
+ *openシステムコール
+ */
 int do_open(char *pathname, u32_t flags);
+
+/*
+ *seekシステムコール
+ */
 off_t do_seek(int fd, off_t offset, int whence);
+
+/*
+ *statシステムコール
+ */
 u32_t do_stat(int fd, u32_t *box);
+
+/*
+ *closeシステムコール
+ */
 i32_t do_close(i32_t fd);
 
+/*
+ *get_keyシステムコール
+ */
 int do_get_key();
 
 #endif
