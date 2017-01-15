da860c791974635b4b8ba98a460eb3ffa76bf8da
AkihiroTakai <at.sisy@gmail.com>
2017-01-15 15:46:54 +0900

    memcpy関数をstring系のファイルに移動

--- include/string.h
+++ include/string.h
@@ -1,11 +1,14 @@
 #ifndef STRING_H
 #define STRING_H
 
+#include "types.h"
+
 int strcmp(char *s, char *t);
 int strlen(const char *str);
 void string_getNext(char *str, char *dst);
 void cut_string(char *str, char *dst, int start_point);
 char *strcpy(char *dst, const char *str);
 char *strcat(char *s1, const char *s2);
+void *memcpy(void *s1, const void *s2, size_t size);
 
 #endif
--- lib/string/string.c
+++ lib/string/string.c
@@ -67,3 +67,29 @@ char *strcat(char *s1, const char *s2) {
 
 	return s1;
 }
+
+/*
+ *=======================================================================================
+ *memcpy関数
+ *指定しただけ、メモリの内容をコピーする関数
+ *引数
+ *void *s1
+ *コピー
+ *const void *s2
+ *コピー先
+ *size_t size
+ *コピーするサイズ
+ *=======================================================================================
+ */
+void *memcpy(void *s1, const void *s2, size_t size){
+	char *ss1 = s1;
+	const char *ss2 = s2;
+
+	if(size != 0){
+		register const char *t = ss2 + size;
+		do{
+			*ss1++ = *ss2++;
+		}while (ss2 != t);
+	}
+	return s1;
+}
