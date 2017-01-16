a2d65267641e84979c3233dd2ee99e631343fd6f
AkihiroTakai <at.sisy@gmail.com>
2017-01-16 15:42:08 +0900

    シェルに入力された文字列にリダイレクトのコマンドがあるか判定する関数を追加

--- include/string.h
+++ include/string.h
@@ -5,7 +5,7 @@
 
 int strcmp(char *s, char *t);
 int strlen(const char *str);
-void string_getNext(char *str, char *dst);
+char *string_getNext(char *str, char *dst);
 void cut_string(char *str, char *dst, int start_point);
 char *strcpy(char *dst, const char *str);
 char *strcat(char *s1, const char *s2);
--- yrsh/reco_redirect.c
+++ yrsh/reco_redirect.c
@@ -5,11 +5,23 @@
  *=======================================================================================
  *ShRecoRedirect関数
  *コマンドにリダイレクトが含まれているか判定する関数
+ *引数
+ *char *line
+ *シェルに入力された一行
  *=======================================================================================
  */
 u8_t ShRecoRedirect(char *line){
 	char *word = (char *)memory_alloc(memman, 128);
 
-	string_getNext(line, word);
+	/*
+	 * > が登場するまでループ
+	 */
+	while(string_getNext(line, word)){
+		if(strcmp(word, ">")){
+			puts("Redirect!!");
+			return 1;
+		}
+	}
+
 	return 0;
 }
