64b96aba72ad6d6c495197263b8b1249f11772f8
AkihiroTakai <at.sisy@gmail.com>
2017-01-19 20:57:48 +0900

    ELFファイル中のセクションを検索する関数を定義

--- kernel/analy_elf.c
+++ kernel/analy_elf.c
@@ -1,4 +1,5 @@
 #include "../include/elf.h"
+#include "../include/string.h"
 
 /*
  *=======================================================================================
@@ -44,11 +45,62 @@ u32_t GetELFDataSize(struct Elf32_info *elf){
 	u32_t dsize = 0;
 	struct Elf32_Phdr* phdr = CutELFProgramHeader(elf);
 
-	for (i = 0; i < elf->e_phnum; i++) {
-		if (!(phdr[i].p_flags & __PF_X__) && limit_addr < phdr[i].p_vaddr) {
+	for(i = 0; i < elf->e_phnum; i++){
+		if(!(phdr[i].p_flags & __PF_X__) && limit_addr < phdr[i].p_vaddr){
 			limit_addr = phdr[i].p_vaddr;
 			dsize = limit_addr + phdr[i].p_memsz;
 		}
 	}
 	return dsize;
+}
+
+/*
+ *=======================================================================================
+ *CopyELFDataSe関数
+ *ELFのデータをデータセグメントにコピーする関数
+ *=======================================================================================
+ */
+void CopyELFDataSe(void *DataSegment, struct Elf32_info *elf){
+	struct Elf32_Phdr *phdr = CutELFProgramHeader(elf);
+	int i;
+	for (i = 0; i < elf->e_phnum; i++){
+		if(!(phdr[i].p_flags & __PF_X__) && phdr[i].p_filesz > 0){
+			/*
+			 *データセグメントにコピー
+			 */
+			memcpy((char *)DataSegment + phdr[i].p_vaddr, (char *)elf + phdr[i].p_offset, phdr[i].p_filesz);
+		}
+	}
+}
+
+/*
+ *=======================================================================================
+ *FindELFSection関数
+ *指定したセクションがELFファイル中に存在するか調べる関数
+ *=======================================================================================
+ */
+struct Elf32_Shdr *FindELFSection(struct Elf32_info *elf, char *SectionName){
+
+	struct Elf32_Shdr* shdr = CutELFSectionHeader(elf);
+	char *strtab =  ((char *)elf + CutELFSectionHeader(elf)[(elf)->e_shstrndx].sh_offset);
+	int i;
+
+	for(i = 0; i < elf->e_shnum; i++){
+		if(strcmp(SectionName, strtab + shdr[i].sh_name) == 0)  //検証
+			return &shdr[i];
+	}
+	return 0;
+}
+
+/*
+ *=======================================================================================
+ *ELFのスタックポインタの位置を返す関数
+ *=======================================================================================
+ */
+u32_t GetELFEsp(struct Elf32_info *elf){
+	struct Elf32_Shdr* shdr = FindELFSection(elf, ".stack");
+	if(shdr)            //見つかった
+		return shdr->sh_size;
+	else                //見つからなかった
+		return 0;
 }
